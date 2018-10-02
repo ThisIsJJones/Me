4377995f6a1dc0d4251658bdeb852919fd3528d3
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2018-10-02 14:40:33 -0500

    remove 2 headers

--- php/email.php
+++ php/email.php
@@ -11,12 +11,10 @@
     
     $email_to = '21jrjones21@gmail.com'; 
     $email_subject = "Message from website";
-    $email_body = "You have received a new message from $name. Here is the message: $message.";
+    $email_body = "You have received a new message from $name. \r\n Here is the message:\r\n\r\n $message.";
 
     $headers = array("From: $visitor_email",
-        "Reply-To: $visitor_email",
-        "X-Mailer: PHP/" . PHP_VERSION,
-        "Content-Type: text/html; charset=ISO-8859-1"
+        "Reply-To: $visitor_email"
         );
     $headers = implode("\r\n", $headers);
 
