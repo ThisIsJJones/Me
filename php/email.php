52b10f84176abbaa590c2172ef3932a946960a29
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2019-02-04 21:53:58 -0600

    mail

--- php/email.php
+++ php/email.php
@@ -11,12 +11,18 @@
     
     $email_to = '21jrjones21@gmail.com'; 
     $email_subject = "Message from website";
-    $email_body = "You have received a new message from $name. \r\n Here is the message:\r\n\r $message.";
+    $email_body = "You have received a new message from: $name. \r\n\r\n Here is the message:\r\n\r\n $message.";
 
-    $headers = array("From: $visitor_email",
-        "Reply-To: $visitor_email"
-        );
-    $headers = implode("\r\n", $headers);
+//    $headers = array("From: $visitor_email",
+//                    "Reply-To: $visitor_email"
+//                    );
+//
+//    $headers = implode("\r\n", $headers);
+
+    $headers = "From: $visitor_email" . "\r\n";
+
+    // use wordwrap() if lines are longer than 70 characters
+    $email_body = wordwrap($email_body,70);
 
     mail($email_to, $email_subject, $email_body, $headers);
     header("location:../index.html");
