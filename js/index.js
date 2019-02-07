64aa3dc16bb468df8fad39826a62b49198b93e54
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2019-02-07 10:43:05 -0600

    animate modal

--- js/index.js
+++ js/index.js
@@ -38,7 +38,6 @@ function modalCloseFunctions(){
     var span = document.getElementsByClassName("close")[0];
 
     modal.onclick = function() {
-        modal.style.display = "none";
         slideProfileContent();
         showNavigation();
     }
@@ -46,7 +45,6 @@ function modalCloseFunctions(){
     
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-        modal.style.display = "none";
         slideProfileContent();
         showNavigation();
     }
@@ -54,7 +52,6 @@ function modalCloseFunctions(){
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
-            modal.style.display = "none";
             slideProfileContent();
             showNavigation();
         }
@@ -78,6 +75,8 @@ function slideProfileContent(){
     document.getElementById("leftContent").classList.add("slide-in");
     document.getElementById("rightContent").classList.add("slide-in");
     document.getElementById("profilePicture").classList.add("expandPicture");
+    document.getElementById("myModal").classList.remove("slide-in-modal");
+    document.getElementById("myModal").classList.add("slide-out-left");
     document.getElementById("quoteContent").style.opacity = 1;
     document.getElementById('fp-nav').classList.remove("remove");
     document.getElementsByClassName("fp-controlArrow")[1].classList.remove("remove");
