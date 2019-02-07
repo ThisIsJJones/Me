a85da757d99c045e94516f7b01f2653583117364
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2019-02-07 08:24:08 -0600

    .

--- php/email.php
+++ php/email.php
@@ -14,8 +14,9 @@
     $email_body = "You have received a new message from: $name. \r\n Here is the message:\r\n\r\n $message. \r\n\r\n";
 
 
-    $myfile = fopen("email.txt", "w");
+    $myfile = fopen("email.txt", "w") or die("Unable to open file!");
     fwrite($myfile, $email_body);
+    fclose($myfile);
 
 
 //    $headers = array("From: $visitor_email",
