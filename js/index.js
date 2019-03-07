1780ee57b26a67f741e2d7e3a4517044c9c99700
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2019-03-07 10:11:22 -0600

    abilityKey && modalremoval

--- js/index.js
+++ js/index.js
@@ -30,35 +30,6 @@ function createJobTitle(){
     }
 }
 
-function modalCloseFunctions(){
-    // Get the modal
-    var modal = document.getElementById('myModal');
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    modal.onclick = function() {
-        slideProfileContent();
-        showNavigation();
-    }
-
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        slideProfileContent();
-        showNavigation();
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            slideProfileContent();
-            showNavigation();
-        }
-    }
-    
-}
-
 function showNavigation(){
      document.getElementById("fp-nav").classList.remove('removeNavBar');
      document.getElementById("fp-nav").classList.remove('addNavBar');
@@ -75,8 +46,6 @@ function slideProfileContent(){
     document.getElementById("leftContent").classList.add("slide-in");
     document.getElementById("rightContent").classList.add("slide-in");
     document.getElementById("profilePicture").classList.add("expandPicture");
-    document.getElementById("myModal").classList.remove("slide-in-modal");
-    document.getElementById("myModal").classList.add("slide-out-left");
     document.getElementById("quoteContent").style.opacity = 1;
     document.getElementById('fp-nav').classList.remove("remove");
     document.getElementsByClassName("fp-controlArrow")[1].classList.remove("remove");
