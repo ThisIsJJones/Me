94e4ba4f359c174426f87cbd2bf6d2247173c40e
Jordan Jones <jordanjones@Jordans-MacBook-Pro.local>
2019-05-19 16:41:51 -0500

    spacing

--- php/email.php
+++ php/email.php
@@ -13,7 +13,7 @@
     $email_subject = "Message from website";
     $date_rcvd = date("Y/m/d");
 
-    $email_body = "New message: \n    Name:   $name \n    Email:    $visitor_email \n  Date:    $date_rcvd  \n   Message:\n        $message \n\n";
+    $email_body = "New message: \n    Name:   $name \n    Email:    $visitor_email \n    Date:    $date_rcvd  \n   Message:\n        $message \n\n";
 
 
     $myfile = fopen("./email.txt", "a+");
